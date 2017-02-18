6300337f71db050cbcb3776b7e4126722ba4460b
Lars Z <womburger@googlemail.com>
2017-02-18 16:22:47 +0100

    Bug Patch for PHP 7.0
    PHP Warnings in Strict Mode
    Declaration of all posible Parameters

--- index.php
+++ index.php
@@ -16,15 +16,19 @@ require_once('config.php');
 // Extract the arguments from the path.
 $path = explode('/', strtok($_SERVER['REQUEST_URI'], '?'));
 $self = explode('/', $_SERVER['PHP_SELF']);
-$args = array_slice($path, end(array_keys($self)));
+$hlpvar_01 = array_keys($self);
+$args = array_slice($path, end($hlpvar_01));
+$args_0 = isset($args[0]) ? $args[0] : "";
+$args_1 = isset($args[1]) ? $args[1] : "";
 
 // Break apart the path to find the endpoint.
-$action = isset($_REQUEST['action']) ? strtolower($_REQUEST['action']) : strtolower($args[0]);
-$uuid   = isset($_REQUEST['uuid']) ? $_REQUEST['uuid'] : $args[1];
-$fields = $_REQUEST['fields'];
+$action = isset($_REQUEST['action']) ? strtolower($_REQUEST['action']) : strtolower($args_0);
+$uuid   = isset($_REQUEST['uuid']) ? $_REQUEST['uuid'] : $args_1;
+$fields = isset($_REQUEST['fields']) ? $_REQUEST['fields'] : "";
+
 
 // Verbose and reverse can be 'true', 'yes', or 1.
-$verbose = in_array(strtolower($_REQUEST['verbose']), array('yes', 'true', 1));
+$verbose = isset($_REQUEST['verbose']) ? in_array(strtolower($_REQUEST['verbose']), array('yes', 'true', 1)) : FALSE;
 
 // Set defaults for the separators.
 $separators = $_REQUEST['separators'];
@@ -32,7 +36,9 @@ $separators[0] = empty($separators[0]) ? '&' : $separators[0];
 $separators[1] = empty($separators[1]) ? '=' : $separators[1];
 
 // Check authentication.
-if ($action != 'install' && $_REQUEST['secret'] != $secret) {
+$REQ_secret = isset($_REQUEST['secret']) ? $_REQUEST['secret'] : "";
+if ($action != 'install' && $REQ_secret != $secret) {
+
 	die("ERROR: NOT AUTHENTICATED");
 }
 
